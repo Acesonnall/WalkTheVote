d5c9b22c7206e3953a1e93eb1dec835f0cda2f51
Paulo Nascimento <pnascimento@uchicago.edu>
2020-09-10 06:55:15 -0700

    batch of swing state scrapers (#5)
    * north carolina done; florida init
    
    * Revert "north carolina done; florida init"
    
    This reverts commit 8d7cf96eae622908a26e392928c2666bfefa2249.
    
    * north carolina scraper
    
    * ohio scraper
    
    * maine scraper
    
    * Adding end of line character
    
    Adding new line to end of file to comply with PEP8 rules.
    
    * Adding end of line character
    
    Adding new line to end of file to comply with PEP8 rules.
    
    Co-authored-by: riya <riyaagar96@gmail.com>
    Co-authored-by: Omar Taylor <omtaylor@iastate.edu>

---
+++ scrapers/maine/maine_scraper.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup 
+import re 
+import json 
+import csv
+import io
+import urllib3
+import pandas as pd
+
+t_file = "https://www.maine.gov/tools/whatsnew/index.php?topic=cec_clerks_registrars&v=text"
+
+url_data = pd.read_table(t_file, sep = '|')
+
+county_info = pd.DataFrame(url_data)
+
+init_info = county_info.columns.tolist() 
+new_init_info = []
+
+for string in init_info: 
+    new_string = string.replace("<plaintext>Abbot", "Abbot")
+    new_init_info.append(new_string)
+
+phone = county_info['(207) 876-3198'].tolist()
+county = county_info['<plaintext>Abbot'].tolist()
+add1 = county_info['133 Main Road'].tolist()
+add2 = county_info['Abbot, ME  04406'].tolist()
+
+county.insert(0, "Abbot")
+add1.insert(0, "133 Main Road")
+add2.insert(0, "Abbot, ME 04406")
+phone.insert(0, "(207) 876-3340")
+
+address = [i + " " + j for i, j in zip(add1, add2)]
