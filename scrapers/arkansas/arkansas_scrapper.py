f654ffee2fff367a2a3579c253461e6bba29851e
Omar Taylor <omtaylor@iastate.edu>
2020-09-12 03:12:32 -0500

    Iowa is done. As it stands, you will need to download one or two things to get it to work on your OS. For Arkansas scrapper, I sectioned everything out into individual functions to reduce complexity. Will document methods at a later time.

--- scrapers/arkansas/arkansas_scrapper.py
+++ scrapers/arkansas/arkansas_scrapper.py
@@ -1,34 +1,30 @@
 import re
 from pprint import pprint
-from typing import List
+from typing import List, Tuple
 
 import requests
 from pdfreader import SimplePDFViewer, PDFDocument
 
-# Get the PDF
-r = requests.get("https://www.sos.arkansas.gov/uploads/elections/ARCountyClerks.pdf")
-
-# Pass byte stream to PDFDocument parser (used for iterating through pages)
-doc = PDFDocument(r.content)
-# Pass byte stream to PDF viewer (used for reading strings on pages)
-viewer = SimplePDFViewer(r.content)
-
 # prep variable that stores final results
-info = []
+ELECTION_OFFICE_INFO = []
 
 
-def fix_line(line: str) -> str:
+def fix_line(line: str, string_list: List[str], index: int) -> str:
     """
     Helper method to apply fix to lines and splice the fix in
+    @param index: position to start amending from
+    @param string_list: master list of strings to be amended
     @param line: line to be fixed and spliced in
     @return: fixed line
     """
-    line = line + page_strings[idx + 1]
-    page_strings[idx: idx + 2] = [line]
+    line = line + string_list[index + 1]
+    string_list[index : index + 2] = [line]
     return line
 
 
-def anomaly_check(line: str, check_type: str = "DEFAULT") -> str:
+def anomaly_check(
+    line: str, strings_list: List[str], index: int, check_type: str = "DEFAULT"
+) -> str:
     """
     Check for edge cases not originally caught when pre-processing the document page.
 
@@ -38,6 +34,8 @@ def anomaly_check(line: str, check_type: str = "DEFAULT") -> str:
     values that are one word long, look one more index ahead and merge those index
     values. Other anomaly is similar to the last, but it's different in detection. I
     detect that anomaly by making sure an address is complete with a zip code.
+    @param index: position to amend from if necessary
+    @param strings_list: master list of stings to be amended if anomaly detected
     @param line: line to be checked
     @param check_type: type of check (default or zip code check)
     @return: Corrected line
@@ -45,58 +43,60 @@ def anomaly_check(line: str, check_type: str = "DEFAULT") -> str:
     # check for address anomaly
     if check_type == "ZIP":
         if not re.search(r"[0-9]{5}", line):
-            line = fix_line(line)
+            line = fix_line(line=line, string_list=strings_list, index=index)
     # otherwise check for the usual anomaly
     else:
         if line and " " not in line:
-            line = fix_line(line)
+            line = fix_line(line=line, string_list=strings_list, index=index)
     return line
 
 
-for i, page in enumerate(doc.pages(), 1):
-    # navigate to page
-    viewer.navigate(i)
-    # render the page
-    viewer.render()
-
-    # collapse that ass
-    page_strings: List[str] = viewer.canvas.strings.copy()
-
+def get_line_ranges(strings_list: List[str]) -> List[Tuple[int, int]]:
     # variable used to define the ranges of strings to be merged together
     from_to = 0
 
     # for each index and page string of the malformed pdf page strings, create tuple
     # that defines the range of indices with strings to be joined together. '" "'
     # designates a new line.
-    merge_ranges = [
-        (from_to, from_to := idx)  # (start, end); start become end as end gets updated
-        for idx, page_str in enumerate(page_strings)
+    return [
+        (from_to, from_to := idx)
+        # (start, end); start become end as end gets updated
+        for idx, page_str in enumerate(strings_list)
         if page_str == " "
     ]
 
+
+def establish_uniformity(
+    strings_list: List[str], line_range_list: List[Tuple[int, int]]
+):
     # repair the page strings in reverse
-    for start, end in merge_ranges[::-1]:
+    for start, end in line_range_list[::-1]:
         # merging values within a range
-        repaired_line = "".join(page_strings[start:end])
+        repaired_line = "".join(strings_list[start:end])
         if repaired_line == " ":
             # delete for the sake of uniformity
-            del page_strings[start:end]
+            del strings_list[start:end]
         else:
             # slice in repaired string in place of range of malformed strings
-            page_strings[start:end] = [repaired_line]
+            strings_list[start:end] = [repaired_line]
+    return strings_list
 
-    # remove unnecessary empty index
-    page_strings.pop()
 
+def get_county_election_office_info(strings_list: List[str]):
+    """
+    Read strings on page and parser out the county, address, and phone numbers. Each
+    county clerk office's contact info is uniform so we can use a modulus operator
+    too look up exact info.
+    @param strings_list: List of strings to be parsed
+    """
     # prep helper vars
     county_name, county_addr, phone = ("", "", "")
 
-    # read strings on page and parser out the county, address, and phone numbers.
-    # Each county clerk office's contact info is uniform so we can use a modulus
-    # operator too look up exact info.
-    for idx, page_line in enumerate(page_strings):
+    for idx, page_line in enumerate(strings_list):
         # check for fucked up shit that slipped through the cracks
-        page_line = anomaly_check(page_line.strip())
+        page_line = anomaly_check(
+            line=page_line.strip(), strings_list=strings_list, index=idx
+        )
 
         # Get county name
         if idx % 7 == 0:
@@ -107,7 +107,12 @@ for i, page in enumerate(doc.pages(), 1):
         # Get remainder of address
         elif idx % 7 == 3:
             # check for zip code anomaly
-            page_line = anomaly_check(page_line.strip(), "ZIP")
+            page_line = anomaly_check(
+                line=page_line.strip(),
+                strings_list=strings_list,
+                check_type="ZIP",
+                index=idx,
+            )
             county_addr += f" {page_line.strip()}"
         # Get phone number
         elif idx % 7 == 4:
@@ -117,9 +122,47 @@ for i, page in enumerate(doc.pages(), 1):
             else:
                 raise Exception("Phone number not found. Fatal.")
 
-            info.append(
+            ELECTION_OFFICE_INFO.append(
                 {"county_name": county_name, "county_addr": county_addr, "phone": phone}
             )
         continue
 
-pprint(info)
+
+def navigate_pages(doc: PDFDocument, viewer: SimplePDFViewer):
+    for i, page in enumerate(doc.pages(), 1):
+        # navigate to page
+        viewer.navigate(i)
+        # render the page
+        viewer.render()
+
+        # collapse that ass
+        page_strings: List[str] = viewer.canvas.strings.copy()
+
+        merge_ranges = get_line_ranges(strings_list=page_strings)
+
+        page_strings = establish_uniformity(
+            strings_list=page_strings, line_range_list=merge_ranges
+        )
+
+        # remove unnecessary empty index
+        page_strings.pop()
+
+        get_county_election_office_info(strings_list=page_strings)
+
+
+def main():
+    # Get the PDF
+    r = requests.get(
+        "https://www.sos.arkansas.gov/uploads/elections/ARCountyClerks.pdf"
+    )
+
+    # Pass byte stream to PDFDocument parser (used for iterating through pages)
+    doc = PDFDocument(r.content)
+    # Pass byte stream to PDF viewer (used for reading strings on pages)
+    viewer = SimplePDFViewer(r.content)
+    navigate_pages(doc, viewer)
+    pprint(ELECTION_OFFICE_INFO)
+
+
+if __name__ == "__main__":
+    main()
