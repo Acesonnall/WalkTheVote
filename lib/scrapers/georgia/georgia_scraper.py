f46ceda8ee9dd4da3f7b71bcc7a27c4af97e5867
riyaagar96 <66572557+riyaagar96@users.noreply.github.com>
2020-10-05 18:21:15 -0700

    removed lib in git ignore and putting scrapers in lib...?

--- lib/scrapers/georgia/georgia_scraper.py
+++ lib/scrapers/georgia/georgia_scraper.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import asyncio
 import os
 import time
@@ -72,6 +73,13 @@ def format_address_data(address_data, county_name, is_physical, mailing_addr={})
 
 
 def format_address_html(info_str):
+=======
+import requests
+from bs4 import BeautifulSoup
+
+
+def format_address(info_str):
+>>>>>>> 5600ab1... removed lib in git ignore and putting scrapers in lib...?
     info_str = info_str.replace("\n<br/>", " ")
     info_str = info_str.replace("<br/>\n", " ")
     info_str = info_str.replace("<br/>", " ")
@@ -97,6 +105,7 @@ def get_phone_number(info_str):
             phone_num.append(c)
         else:
             break
+<<<<<<< HEAD
 
     return "".join(phone_num)
 
@@ -238,3 +247,51 @@ if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(get_georgia_election_offices())
     end = time.time()
     print(f"{bcolors.OKBLUE}Completed in {end - start} seconds.{bcolors.ENDC}")
+=======
+    print(phone_num)
+    return "".join(phone_num)
+
+
+f = open("./log.txt", "w")
+
+# Get list of county names from registrar to populate form
+registrar_url = "https://elections.sos.ga.gov/Elections/countyregistrars.do"
+r = requests.get(registrar_url)
+soup = BeautifulSoup(r.content, "html5lib")
+county_option_list = soup.findAll(attrs={"name": "idTown"})[0].findAll("option")
+
+id_list = [county_option["value"] for county_option in county_option_list]
+county_list = [county_option.string for county_option in county_option_list]
+
+# Use list of counties and IDs to get county info for each county
+info_url = "https://elections.sos.ga.gov/Elections/contactinfo.do"
+
+county_info = {}
+
+for i in range(len(id_list)):
+    county_id = id_list[i]
+    county_name = county_list[i]
+    r = requests.post(
+        info_url,
+        data={"idTown": county_id, "SubmitCounty": "Submit", "contactType": "R"},
+    )
+    soup = BeautifulSoup(r.content, "html5lib")
+    table = soup.find("table", {"id": "Table1"})
+    rows = table.find_all("tr")
+
+    phys_info_str, mail_info_str, phone_number = "", "", ""
+
+    if (
+        "Physical Address:" in rows[0].getText()
+        and "SAME AS ABOVE" not in rows[0].getText()
+    ):
+        phys_info_str = str(rows[0])
+        phys_address = format_address(phys_info_str)
+
+    mail_info_str = str(rows[1])
+    mail_address = format_address(mail_info_str)
+
+    if "Telephone: " in rows[2].getText():
+        contact_info_str = rows[2].getText()
+        phone_number = get_phone_number(contact_info_str)
+>>>>>>> 5600ab1... removed lib in git ignore and putting scrapers in lib...?
