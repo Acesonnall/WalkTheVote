9a13102ce6a9ae438786142f4523ab3fb519746d
riyaagar96 <66572557+riyaagar96@users.noreply.github.com>
2020-10-05 19:06:04 -0700

    Arizona phase 2 complete

--- lib/scrapers/arizona/arizona_scraper.py
+++ lib/scrapers/arizona/arizona_scraper.py
@@ -163,6 +163,7 @@ for i in county_info_text:
 clerk_name = [i.split('\n\t\t\t\t')[0] for i in actual_info]
 building_name = [i.split('\n\t\t\t\t')[1] for i in actual_info]
 address_1 = [i.split('\n\t\t\t\t')[2].replace("Physical: ", "") for i in actual_info]
+address_1 = [i.split(' (')[0] for i in address_1]
 address_2 = []
 for i in actual_info:
     if 'Physical:' in i:
@@ -182,51 +183,55 @@ address_2 = flatten(address_2)
 
 for n, i in enumerate(address_2):
     if i == 'Phone - 520-724-4330':
-        address_2[n] = 'Tucson, Arizona 85702'
+        address_2[n] = ''
+    if i == 'Suite 101':
+        address_2[n] = 'Suite 101, Nogales, Arizona 85621'
+
+full_address = [a + ', ' + b for a, b in zip(address_1, address_2)]
+
+def Find(string):
+    # findall() has been used
+    # with valid conditions for urls in string
+    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    url = re.findall(regex, string)
+    return [x[0] for x in url]
+
+websites = [Find(i) for i in county_info_text]
+websites = [item for sublist in websites for item in sublist][::3]
+websites.remove('http://recorder.maricopa.gov/elections/')
 
 masterList = []
 
 def formatAddressData(address, countyName):
     mapping = electionsaver.addressSchemaMapping
+
     parsedDataDict = usaddress.tag(address, tag_mapping=mapping)[0]
 
-    try:
-        finalAddress = {
-            "city": parsedDataDict["city"],
-            "state": parsedDataDict['state'],
-            "zipCode": parsedDataDict['zipCode'],
-        }
-    except:
-        print(f'Error with data for {countyName} county, data is {parsedDataDict}')
-
-    return finalAddress
-
-def formatStreetNumber(streetNumberName, countyName):
-    mapping = electionsaver.addressSchemaMapping
-    parsedDataDict = usaddress.tag(streetNumberName, tag_mapping=mapping)[0]
-
     finalAddress = {
-        "streetNumberName": parsedDataDict['streetNumberName'],
+        "state": "Arizona",
+        "zipCode": parsedDataDict['zipCode'],
     }
-
+    if 'streetNumberName' in parsedDataDict:
+        finalAddress['streetNumberName'] = parsedDataDict['streetNumberName']
+    if 'city' in parsedDataDict:
+        finalAddress['city'] = parsedDataDict['city']
+    if 'poBox' in parsedDataDict:
+        finalAddress['poBox'] = parsedDataDict['poBox']
+    if 'locationName' in parsedDataDict:
+        finalAddress['locationName'] = parsedDataDict['locationName']
     if 'aptNumber' in parsedDataDict:
         finalAddress['aptNumber'] = parsedDataDict['aptNumber']
-
     return finalAddress
 
-
 for i in range(len(county_names)):
-    addressSchema = formatAddressData(address_2[i], county_names[i])
-    streetSubSchema = formatStreetNumber(address_1[i], county_names[i])
-    addressSchema['streetNumberName'] = streetSubSchema['streetNumberName']
-    if 'aptNumber' in streetSubSchema:
-        addressSchema['aptNumber'] = streetSubSchema['aptNumber']
+    addressSchema = formatAddressData(full_address[i], county_names[i])
     schema = {
         "countyName": county_names[i].strip(),
         "physicalAddress": addressSchema,
         "phone": phone_num[i],
         "officeSupervisor": clerk_name[i],
-        "supervisorTitle": building_name[i]
+        "supervisorTitle": building_name[i],
+        "website": websites[i]
         }
     masterList.append(schema)
 
